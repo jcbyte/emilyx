4a95251067739747a04737b880dcb0b45fd0e72a
Joel Cutler <joelcutler108@gmail.com>
2025-05-06 23:42:08 +0100

    get timestamp api

--- api/timestamp.js
+++ api/timestamp.js
@@ -1,14 +1,19 @@
-let timestamp = null;
+const date = new Date(2025, 5, 6, 18, 0, 0);
+let timestamp = date.getTime();
 
 export default async function (req, res) {
 	// If POST then set the timestamp
 	if (req.method === "POST") {
+		// todo allow to set timestamp
+		return res.status(500).json({ message: "Not yet implemented" });
+
 		// Extract the timestamp
 		const { timestamp: newTimestamp } = req.body;
 
-		if (customTimestamp) {
+		if (newTimestamp) {
 			// Save the custom timestamp (ensure it's a valid date string)
-			timestamp = new Date(customTimestamp).toISOString();
+			timestamp = new Date(newTimestamp).toISOString();
+
 			return res.status(200).json({ message: "Timestamp saved", timestamp });
 		} else {
 			return res.status(400).json({ message: "Timestamp is required" });
@@ -18,6 +23,7 @@ export default async function (req, res) {
 	// If GET then retrieve the timestamp
 	if (req.method === "GET") {
 		// Send the timestamp if it exists
+
 		if (timestamp) {
 			return res.status(200).json({ timestamp });
 		}
