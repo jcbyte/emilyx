004587f67ac44a0dd46ca42773e429322c10c256
Joel Cutler <joelcutler108@gmail.com>
2025-06-25 22:54:52 +0100

    helper script to get OAuth token

---
+++ scripts/getToken.js
@@ -0,0 +1,44 @@
+import { google } from "googleapis";
+import readline from "readline";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+async function getAccessToken() {
+	const authClient = new google.auth.OAuth2({
+		clientId: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		redirectUri: "http://localhost:3000",
+	});
+
+	const authUrl = authClient.generateAuthUrl({
+		accessType: "offline",
+		scope: ["https://www.googleapis.com/auth/calendar.readonly"],
+	});
+
+	console.log("Authorize this app by visiting this url:", authUrl);
+
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	rl.question("Enter the redirect URL: ", async (url) => {
+		rl.close();
+
+		const inputUrl = new URL(url);
+		const code = inputUrl.searchParams.get("code");
+
+		if (!code) {
+			throw new Error("Unable to extract code");
+		}
+
+		const { tokens } = await authClient.getToken(code);
+		authClient.setCredentials(tokens);
+
+		console.log();
+		console.log("Token:", tokens);
+	});
+}
+
+getAccessToken();
