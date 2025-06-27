f6354096422bb2a971a7d61dc2024073647040fd
Joel Cutler <joelcutler108@gmail.com>
2025-06-27 11:54:49 +0100

    removed firebase from project

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,9 @@
 export async function getNextEvent(): Promise<null | Date> {
 	type EventAPIResponse = { event: false } | { event: true; start: string };
 
-	console.log("call");
-
 	const res = (await fetch("/api/getNextEvent", {
 		method: "GET",
 	}).then((res) => res.json())) as EventAPIResponse;
-
-	console.log("return", res);
 
 	if (!res.event) return null;
 
