a67163c44a6545a7a75c4e814a594a6d5f342638
Joel Cutler <joelcutler108@gmail.com>
2025-09-03 23:09:13 +0100

    modify how heartbeats/time is calculated

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,42 @@ import AnimatedLimes from "./components/AnimatedLimes";
 
 const BPM = 70;
 
+interface Time {
+	days: number;
+	hours: number;
+	minutes: number;
+	seconds: number;
+}
+
 export default function App() {
-	const [heartbeats, setHeartbeats] = useState<number>(0);
-	const [heartbeatState, setHeartbeatState] = useState<"loading" | "counting" | "none" | "error">("loading");
+	const [nextEvent, setNextEvent] = useState<Date | null>(null);
+	const [heartbeatState, setHeartbeatState] = useState<"loading" | "event" | "none" | "error">("loading");
 
-	const [isBeating, setIsBeating] = useState<boolean>(false);
-
-	async function loadHeartbeats() {
-		// Get start time of next event from calendar
-		const nextEvent = await getNextEvent();
-		if (nextEvent === "error") {
-			setHeartbeatState("error");
-			return;
-		}
-
-		if (!nextEvent) {
-			setHeartbeatState("none");
-		} else {
-			// Calculate heartbeats remaining
-			const remaining = nextEvent.getTime() - Date.now();
-			const remainingHeartbeats = (remaining / 1000 / 60) * BPM;
-			setHeartbeats(remainingHeartbeats);
-			setHeartbeatState("counting");
-		}
-	}
-
-	function handlePageFocus() {
-		// Reload timestamp when coming back to the page
-		loadHeartbeats();
-	}
+	const [heartbeatsUntil, setHeartbeatsUntil] = useState<number>(0);
+	const [timeUntil, setTimeUntil] = useState<Time>({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
 	// Load timestamp
 	useEffect(() => {
+		async function loadHeartbeats() {
+			// Get next event from calendar
+			const nextEventTime = await getNextEvent();
+			if (!nextEventTime.ok) {
+				setHeartbeatState("error");
+				return;
+			}
+
+			if (nextEventTime.data === null) setHeartbeatState("none");
+			else setHeartbeatState("event");
+			setNextEvent(nextEventTime.data);
+		}
+
 		// Initially load timestamp
 		loadHeartbeats();
+
+		function handlePageFocus() {
+			// Reload timestamp when coming back to the page
+			loadHeartbeats();
+		}
 
 		window.addEventListener("focus", handlePageFocus);
 		return () => {
@@ -47,35 +49,69 @@ export default function App() {
 		};
 	}, []);
 
-	// Heart beat animation effect
-	useEffect(() => {
-		if (heartbeatState === "counting") {
-			const beatInterval = setInterval(() => {
-				setIsBeating(true);
-				setHeartbeats((prev) => prev! - 1);
-				setTimeout(() => setIsBeating(false), 200);
-			}, (60 / BPM) * 1000);
-
-			return () => clearInterval(beatInterval);
-		}
-	}, [heartbeatState]);
-
-	function getTimeStr(heartbeats: number): string {
-		const hours = Math.floor(heartbeats / BPM / 60);
-		const minutes = Math.floor((heartbeats / BPM) % 60);
-
-		const hoursStr = hours <= 0 ? null : `${hours} hour${hours > 1 ? "s" : ""}`;
-		const minutesStr = minutes <= 0 ? null : `${minutes} minute${minutes > 1 ? "s" : ""}`;
-
-		// If time <= 1 minute
-		if (!hoursStr && !minutesStr) {
-			// If time <= 0
-			if (heartbeats <= 0) return "This moment was worth every beat";
-			return "That's less than a minute more!";
-		}
-
-		return `That's only ${hoursStr ?? ""}${hoursStr && minutesStr ? " and " : ""}${minutesStr ?? ""} more!`;
+	function getTimeRemaining(): number | null {
+		// Get time until even in milliseconds
+		if (!nextEvent) return null;
+		return nextEvent.getTime() - Date.now();
 	}
+
+	function getHeartbeatsUntil(): number | null {
+		const mills = getTimeRemaining();
+		if (!mills) return null;
+
+		return (mills / 1000 / 60) * BPM;
+	}
+
+	function getTimeUntil(): Time | null {
+		const mills = getTimeRemaining();
+		if (!mills) return null;
+
+		const secondsTotal = Math.floor(mills / 1000);
+
+		const days = Math.floor(secondsTotal / (24 * 60 * 60));
+		const hours = Math.floor((secondsTotal % (24 * 60 * 60)) / (60 * 60));
+		const minutes = Math.floor((secondsTotal % (60 * 60)) / 60);
+		const seconds = secondsTotal % 60;
+
+		return { days, hours, minutes, seconds };
+	}
+
+	useEffect(() => {
+		let heartbeatInterval: NodeJS.Timeout | null = null;
+		let timeInterval: NodeJS.Timeout | null = null;
+
+		if (heartbeatState === "event") {
+			function updateHeartbeatsUntil() {
+				const heartbeats = getHeartbeatsUntil();
+				if (!heartbeats) return;
+
+				setHeartbeatsUntil(heartbeats);
+			}
+
+			function updateTimeUntil() {
+				const time = getTimeUntil();
+				if (!time) return;
+
+				setTimeUntil(time);
+			}
+
+			updateHeartbeatsUntil();
+			updateTimeUntil();
+
+			heartbeatInterval = setInterval(updateHeartbeatsUntil, (60 / BPM) * 1000);
+			timeInterval = setInterval(updateTimeUntil, 1000);
+		}
+
+		return () => {
+			if (heartbeatInterval) clearInterval(heartbeatInterval);
+			if (timeInterval) clearInterval(timeInterval);
+		};
+	}, [nextEvent, heartbeatState]);
+
+	// todo refresh ui
+	// todo heart animation
+	// todo handle currently in event
+	// todo checkout limes
 
 	return (
 		<>
@@ -85,16 +121,11 @@ export default function App() {
 				</span>
 
 				<div className="relative">
-					<Heart
-						className={`h-48 w-48 fill-pink-400 stroke-pink-600 transition-all duration-200 ${
-							isBeating ? "scale-110" : "scale-100"
-						}`}
-					/>
+					<Heart className="h-48 w-48 fill-pink-400 stroke-pink-600 transition-all duration-200" />
 					<div className="absolute inset-0 flex items-center justify-center">
-						{/* Bottom padding to make the text "look" more central */}
+						{/* Bottom padding to make the text look more central */}
 						<span className="text-fuchsia-50 font-bold text-xl pb-4">
-							{heartbeatState === "counting" &&
-								(heartbeats > 0 ? Math.floor(heartbeats).toLocaleString() : "We're Here!")}
+							{heartbeatState === "event" && Math.floor(heartbeatsUntil).toLocaleString()}
 							{heartbeatState === "none" && "Stunner"}
 						</span>
 					</div>
@@ -103,7 +134,8 @@ export default function App() {
 				<div className="flex flex-col gap-2 justify-center items-center">
 					<span className="text-lg text-pink-700 text-center">
 						{heartbeatState === "loading" && "Syncing our heartbeat..."}
-						{heartbeatState === "counting" && getTimeStr(heartbeats)}
+						{heartbeatState === "event" &&
+							`${timeUntil.days}d ${timeUntil.hours}h ${timeUntil.minutes}m ${timeUntil.seconds}s`}
 						{heartbeatState === "none" && "Nothing planned, but I'll see you soon love!"}
 						{heartbeatState === "error" && "Joel's OAuth token has probably expired :("}
 					</span>
